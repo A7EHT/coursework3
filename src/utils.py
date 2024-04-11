d0dd370ed51f86d434fa8352c48adfa39cd54554
A7EHT <takprosto659@gmail.com>
2024-04-11 18:42:07 +0500

    update get_list_json

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
 import json
+import os.path
+from config import ROOT_DIR
 
 
-
+OPERATION_PATH = os.path.join(ROOT_DIR, "data", "operations.json")
 def get_list_json():
-    with open("operations.json") as file:
-        data = json.loads(file)
-        return data
+    with open("../data/operations.json", encoding="utf-8") as file:
+        result = json.load(file)
+        return result
+
 
