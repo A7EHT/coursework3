acecbcd43608654ddbe9e860bf8e29ae9776f884
A7EHT <takprosto659@gmail.com>
2024-04-11 19:00:07 +0500

    add sort_result_json

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,14 @@ from config import ROOT_DIR
 
 
 OPERATION_PATH = os.path.join(ROOT_DIR, "data", "operations.json")
+
+
 def get_list_json():
     with open("../data/operations.json", encoding="utf-8") as file:
         result = json.load(file)
         return result
 
+
+def sort_result_json():
+    return sorted(get_list_json(), key=lambda x: x['date'], reverse=True)
 
